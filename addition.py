afc9390eebc843c4b3d17a3aa9a4efda19b60aea
orooba00 <awan.orooba@student.ccm.edu>
2025-09-26 15:08:03 -0400

    added addition function

---
+++ addition.py
@@ -0,0 +1,8 @@
+"""
+This module provides a function for performing addition operations.
+"""
+def add(a, b):
+    """
+    This function adds two numbers and returns the result.
+    """
+    return a + b
